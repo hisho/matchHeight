aba5897f1f269e634f6fda64e1c846a38e24a7f2
HIsho Ohmasu <>
2020-04-20 00:15:24 +0900

    ♻️ functionをclassに変更

--- src/js/modules/MatchHeight.ts
+++ src/js/modules/MatchHeight.ts
@@ -1,25 +1,38 @@
-function _MatchHeight() {
-  const targetElements: HTMLElement[] = [...document.querySelectorAll<HTMLElement>('[data-match-height]')];
-  if (targetElements) matchHeight(targetElements);
+class MatchHeight {
+  matchHeightElements: HTMLElement[]
+  matchHeightElementDataNameList: string[]
+  matchHeightDataElements: HTMLElement[][]
+  constructor() {
+    this.matchHeightElements = [...document.querySelectorAll<HTMLElement>('[data-match-height]')];
+    this.matchHeightElementDataNameList = this.getElementDataNameList();
+    this.matchHeightDataElements = this.test();
+    this.init();
+  }
+
+  init() {
+    this.matchHeightDataElements.forEach(matchHeightElement => {
+      const heights:number[] = matchHeightElement.map(x => x.clientHeight);
+      const maxHeight:number = Math.max(...heights);
+      matchHeightElement.forEach(x => {
+        console.log(x.offsetTop);
+        (x.style.height = `${maxHeight}px`)
+      });
+    });
+  }
+
+  getElementDataNameList ():string[] {
+    const matchHeightElementDataNames = this.matchHeightElements.map(x => x.dataset.matchHeight);
+    return [...new Set(matchHeightElementDataNames)]
+  }
+
+  test():HTMLElement[][] {
+    return this.matchHeightElementDataNameList.map(dataName => {
+      return this.matchHeightElements.filter(targetElement => {
+        const data:string = targetElement.dataset.matchHeight;
+        if (data === dataName) return targetElement;
+      });
+    });
+  }
 }
 
-function matchHeight(targetElements) {
-  const dataNames: string[] = targetElements.map(x => x.dataset.matchHeight);
-  const dataNameList: string[] = [...new Set(dataNames)];
-  const matchHeightElements: HTMLElement[][] = dataNameList.map(dataName => {
-    return targetElements.filter(targetElement => {
-      const data:string = targetElement.dataset.matchHeight;
-      if (data === dataName) return targetElement;
-    });
-  });
-  matchHeightElements.forEach(matchHeightElement => {
-    const heights:number[] = matchHeightElement.map(x => x.clientHeight);
-    const maxHeight:number = Math.max(...heights);
-    matchHeightElement.forEach(x => {
-      console.log(x.offsetTop);
-      (x.style.height = `${maxHeight}px`)
-    });
-  });
-}
-
-_MatchHeight();
+if(document.querySelector<HTMLElement>('[data-match-height]')) new MatchHeight();
