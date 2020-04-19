f5416e621d9b522c34f4f5e25f191c06a080554f
HIsho Ohmasu <>
2020-04-19 23:09:22 +0900

    ✨ cardをjson化とタイトルと本文をランダムで表示するように変更

--- gulpfile.js
+++ gulpfile.js
@@ -6,24 +6,25 @@ const rename = require('gulp-rename');
 const plumber = require('gulp-plumber');
 const notify  = require('gulp-notify');
 const json = JSON.parse(fs.readFileSync('src/ejs/data/json.json'));
+const cards = JSON.parse(fs.readFileSync('src/ejs/data/card.json'));
 
 const ejs = (cb) => {
   gulp
     .src('src/ejs/**/!(_)*.ejs')
-    // エラー後もwatchを止めない
     .pipe(plumber({
       errorHandler: notify.onError("Error: <%= error.message %>")
     }))
     .pipe(gulpEjs({
+      cards: cards.cards,
       example: json.example
     }))
-    // .ejsを.htmlへリネーム
     .pipe(rename({ extname: '.html' }))
     .pipe(gulp.dest(setting.basedir));
   cb();
 };
 
 exports.ejs = ejs;
+
 exports.watch = () => {
   gulp.watch('src/ejs/**/*.ejs', ejs);
 };
